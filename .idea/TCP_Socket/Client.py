dc30fe84433cb58500401852f7a171184b989727
ArditGjyrevci <ardit.gjyrevci@student.uni-pr.edu>
2023-11-29 21:59:13 +0100

    c2

--- .idea/TCP_Socket/Client.py
+++ .idea/TCP_Socket/Client.py
@@ -6,3 +6,17 @@ ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
 DISCONNECT_MSG = "!DISCONNECT"
+
+def file_type(type):
+    match type:
+        case 'read':
+            f = open("read.txt", "r")
+            print(f.read())
+        case 'write':
+            filename = input("Enter the filename to write to: ")
+            user_input = input(f"Enter the content to write to {filename}: ")
+            print(f"Content to write: {user_input}")
+        case 'execute':
+            print("Execute..")
+
+
