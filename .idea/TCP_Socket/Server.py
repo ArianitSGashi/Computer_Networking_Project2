af18311bb810ee0baad2376225d77e81bb3bd442
Arianit Gashi <98714216+ArianitSGashi@users.noreply.github.com>
2023-11-29 22:01:27 +0100

    Commit3

--- .idea/TCP_Socket/Server.py
+++ .idea/TCP_Socket/Server.py
@@ -8,3 +8,41 @@ ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
 DISCONNECT_MSG = "!DISCONNECT"
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    connected = True
+    while connected:
+        msg = conn.recv(SIZE).decode(FORMAT)
+        if msg == DISCONNECT_MSG:
+            connected = False
+            continue
+
+        print(f"[{addr}] {msg}")
+
+        try:
+            command, *rest = msg.split()
+            if command == "read":
+                # Handle read operation
+                try:
+                    with open(rest[0], "r") as file:
+                        content = file.read()
+                    conn.send(content.encode(FORMAT))
+                except FileNotFoundError:
+                    conn.send("File not found.".encode(FORMAT))
+            elif command == "write" and rest:
+                # Handle write operation
+                filename, content = rest[0], " ".join(rest[1:])
+                with open(filename, "a") as file:
+                    file.write(content + "\n")
+                conn.send("File written successfully.".encode(FORMAT))
+            elif command == "execute" and rest:
+                # Handle execute operation
+                output = os.popen(rest[0]).read()
+                conn.send(output.encode(FORMAT))
+            else:
+                conn.send("Invalid command.".encode(FORMAT))
+        except Exception as e:
+            conn.send(f"Error: {str(e)}".encode(FORMAT))
+
+    conn.close()
